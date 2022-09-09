10e28e21bc806a70d930187a857c82ebdea8a3c4
Vladyslav Harshyn <garshyn_98@ukr.net>
2022-09-09 11:54:03 +0300

    hw done

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,43 @@
-import { nanoid } from 'nanoid';
-import { useState, useEffect } from 'react';
-
 import Section from './components/Section/Section';
 import Form from './components/Form/Form';
 import Contacts from './components/Contacts/Contacts';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { addContacts, removeContacts } from 'redux/items/items-action';
+import { getContacts } from 'redux/items/items-selector';
+import { getFilter } from 'redux/filter/filter-selector';
+import { setFilter } from 'redux/filter/filter-action';
+
 const App = () => {
-  const savedContacts = JSON.parse(localStorage.getItem('contacts'));
-
-  const [contacts, setContacts] = useState(savedContacts ? savedContacts : []);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
 
   const addContact = ({ name, number }) => {
-    const searchName = contacts.map(({ name }) => name);
-
-    if (searchName.includes(name)) {
+    const action = addContacts(name, number);
+    const arrayOfName = contacts.map(contact => contact.name);
+    if (arrayOfName.includes(name)) {
       return alert(`${name} is already in contacts`);
     }
-
-    const id = nanoid();
-    const newContact = { name, number, id };
-
-    setContacts(prevContacts => {
-      return [...prevContacts, newContact];
-    });
+    dispatch(action);
   };
 
   const removeContact = id => {
-    setContacts(prevState => prevState.filter(a => a.id !== id));
+    dispatch(removeContacts(id));
   };
 
   const getFiltredContacts = () => {
-    const normalizedFilter = filter.toLocaleLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
+    const normalizeFilter = filter.toLowerCase();
+    const filterContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter)
     );
+    return filterContacts;
   };
 
-  const filterContacts = event => {
-    // console.log(event.target);
-    setFilter(event.target.value);
-  };
+  // const filterContacts = getFiltredContacts();
 
   return (
-    <div>
+    <div className="phonebook">
       <Section title={'Phonebook'}>
         <Form onSubmit={addContact} />
       </Section>
@@ -55,95 +45,11 @@ const App = () => {
         <Contacts
           removeContact={removeContact}
           getFilteredConatcts={getFiltredContacts()}
-          handleFilter={filterContacts}
+          handleFilter={event => dispatch(setFilter(event.currentTarget.value))}
         />
       </Section>
     </div>
   );
 };
-// class App extends Component {
-//   state = {
-//     contacts: [],
-//     filter: '',
-//   };
-
-//   componentDidMount() {
-//     const ContactsParse = JSON.parse(localStorage.getItem('users'));
-//     if (ContactsParse) {
-//       this.setState({ contacts: ContactsParse });
-//     }
-//   }
-
-//   componentDidUpdate(prevState) {
-//     if (prevState.contacts !== this.state.contacts) {
-//       localStorage.setItem('users', JSON.stringify(this.state.contacts));
-//     }
-//   }
-
-//   addContact = ({ name, number }) => {
-//     const { contacts } = this.state;
-
-//     if (contacts.find(el => el.name === name)) {
-//       alert(`${name} is already in contacts`);
-//     } else {
-//       this.setState(({ contacts }) => {
-//         const user = {
-//           name,
-//           number,
-//           id: nanoid(),
-//         };
-//         return {
-//           contacts: [...contacts, user],
-//         };
-//       });
-//     }
-//   };
-
-//   removeContact = id => {
-//     this.setState(({ contacts }) => {
-//       return {
-//         contacts: contacts.filter(item => item.id !== id),
-//       };
-//     });
-//   };
-
-//   getFilteredConatcts = () => {
-//     const { filter, contacts } = this.state;
-
-//     if (!filter) {
-//       return contacts;
-//     }
-//     const filterValue = filter.toLowerCase();
-//     const filteredContacts = contacts.filter(({ name }) => {
-//       const nameValue = name.toLowerCase();
-//       return nameValue.includes(filterValue);
-//     });
-
-//     return filteredContacts;
-//   };
-
-//   handleFilter = ({ target }) => {
-//     this.setState({
-//       filter: target.value,
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <Section title={'Phonebook'}>
-//           <Form onSubmit={this.addContact} />
-//         </Section>
-//         <Section title={'Contacts'}>
-//           <Contacts
-//             removeContact={removeContact}
-//             getFilteredConatcts={getFilteredConatcts()}
-//             handleFilter={handleFilter}
-//           />
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import { store, persistedStore } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistedStore}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
